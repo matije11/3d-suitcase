e4165e829e475c8c2312731ae8fa22edb8ac31af
matije11 <matije.vukovic@gmail.com>
2024-10-20 12:34:50 +0200

    scroll colors on arrows and header underline

--- src/components/Colors/Colors.jsx
+++ src/components/Colors/Colors.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { CTAContext } from "../../contexts/CTAContext";
 import useIsMobile from "../../hooks/useIsMobile";
 import ColorsMap from "./ColorsMap";
@@ -8,9 +8,22 @@ import "../ColorPicker/ColorPicker.css";
 import useResponsiveCta from "../../hooks/useResponsiveCta";
 
 const Colors = () => {
+  const scrollContainerRef = useRef(null);
   const isMobile = useIsMobile();
   const { responsiveCta } = useResponsiveCta();
   const { active, activeColors } = useContext(CTAContext);
+
+  const scrollLeft = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollLeft -= 150;
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollLeft += 150;
+    }
+  };
 
   return (
     <>
@@ -27,11 +40,21 @@ const Colors = () => {
         <ColorsMap />
       ) : (
         <div className="colors-wrapper-mobile">
-          <img src={arrowLeftMobile} alt="Arrow left mobile" />
-          <div className="scroll-container">
+          <img
+            src={arrowLeftMobile}
+            alt="Arrow left mobile"
+            style={{ marginLeft: "8px" }}
+            onClick={() => scrollLeft()}
+          />
+          <div className="scroll-container" ref={scrollContainerRef}>
             <ColorsMap />
           </div>
-          <img src={arrowRightMobile} alt="Arrow right mobile" />
+          <img
+            src={arrowRightMobile}
+            alt="Arrow right mobile"
+            style={{ marginRight: "8px" }}
+            onClick={() => scrollRight()}
+          />
         </div>
       )}
     </>
--- src/utils/activateModifier.js
+++ src/utils/activateModifier.js
@@ -1,110 +1,54 @@
-var nextButtonFlag = false;
-
 export function activateModifier(value) {
-  if (nextButtonFlag) return;
-
-  nextButtonFlag = true;
-
   switch (value) {
     case "camera_body":
-      window.Unlimited3D.activateModifier(
-        {
-          modifier: "camera_body",
-        },
-        () => {
-          nextButtonFlag = false;
-        }
-      );
+      window.Unlimited3D.activateModifier({
+        modifier: "camera_body",
+      });
       break;
     case "camera_body_mobile":
-      window.Unlimited3D.activateModifier(
-        {
-          modifier: "camera_body_mobile",
-        },
-        () => {
-          nextButtonFlag = false;
-        }
-      );
+      window.Unlimited3D.activateModifier({
+        modifier: "camera_body_mobile",
+      });
       break;
     case "camera_corners":
-      window.Unlimited3D.activateModifier(
-        {
-          modifier: "camera_corners",
-        },
-        () => {
-          nextButtonFlag = false;
-        }
-      );
+      window.Unlimited3D.activateModifier({
+        modifier: "camera_corners",
+      });
       break;
     case "camera_corners_mobile":
-      window.Unlimited3D.activateModifier(
-        {
-          modifier: "camera_corners_mobile",
-        },
-        () => {
-          nextButtonFlag = false;
-        }
-      );
+      window.Unlimited3D.activateModifier({
+        modifier: "camera_corners_mobile",
+      });
       break;
     case "camera_handle":
-      window.Unlimited3D.activateModifier(
-        {
-          modifier: "camera_handle",
-        },
-        () => {
-          nextButtonFlag = false;
-        }
-      );
+      window.Unlimited3D.activateModifier({
+        modifier: "camera_handle",
+      });
       break;
     case "camera_handle_mobile":
-      window.Unlimited3D.activateModifier(
-        {
-          modifier: "camera_handle_mobile",
-        },
-        () => {
-          nextButtonFlag = false;
-        }
-      );
+      window.Unlimited3D.activateModifier({
+        modifier: "camera_handle_mobile",
+      });
       break;
     case "camera_wheels":
-      window.Unlimited3D.activateModifier(
-        {
-          modifier: "camera_wheels",
-        },
-        () => {
-          nextButtonFlag = false;
-        }
-      );
+      window.Unlimited3D.activateModifier({
+        modifier: "camera_wheels",
+      });
       break;
     case "camera_wheels_mobile":
-      window.Unlimited3D.activateModifier(
-        {
-          modifier: "camera_wheels_mobile",
-        },
-        () => {
-          nextButtonFlag = false;
-        }
-      );
+      window.Unlimited3D.activateModifier({
+        modifier: "camera_wheels_mobile",
+      });
       break;
     case "default_camera_desktop":
-      window.Unlimited3D.activateModifier(
-        {
-          modifier: "default_camera_desktop",
-        },
-        () => {
-          nextButtonFlag = false;
-        }
-      );
+      window.Unlimited3D.activateModifier({
+        modifier: "default_camera_desktop",
+      });
       break;
     case "default_camera_mobile":
-      window.Unlimited3D.activateModifier(
-        {
-          modifier: "default_camera_mobile",
-        },
-        () => {
-          nextButtonFlag = false;
-        }
-      );
+      window.Unlimited3D.activateModifier({
+        modifier: "default_camera_mobile",
+      });
       break;
     default:
       break;
